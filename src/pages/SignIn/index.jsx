73e67d4901499a30eccca93ddd0ca27dd1cb9044
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2025-02-01 20:45:31 -0300

    atualização na aparencia

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,41 @@
-export default function SignIn(){
-    return(
-        <div>
-            <h1>Pagina login</h1>
+import { useState } from "react";
+import "./signin.css";
+
+import logo from "../../assets/logo.png";
+import { Link } from 'react-router-dom'
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container-center">
+      <div className="Login">
+        <div className="Login-area">
+          <img src={logo} alt="Logo de sistema de chamados" />
         </div>
-    )
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="**********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit" value="Acessar" />
+        </form>
+
+        <Link to ="register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
